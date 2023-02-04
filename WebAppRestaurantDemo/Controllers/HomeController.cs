7755e72eb2e2934d7a63f61a53e9ea9d7b5f4ea7
Levan Khachidze <56764511+enott47@users.noreply.github.com>
2023-02-04 15:41:12 +0400

    Add project files.

---
+++ WebAppRestaurantDemo/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppRestaurantDemo.Repositories;
+using WebAppRestaurantDemo.Models;
+using WebAppRestaurantDemo.ViewModel;
+
+namespace WebAppRestaurantDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RestaurantDBEntities objrestaurantDBEntities;
+        public HomeController()
+        {
+            objrestaurantDBEntities = new RestaurantDBEntities();
+        }
+
+
+        // GET: Home
+        public ActionResult Index()
+
+
+        {
+            CustomerRepository ObjCustomerRepository = new CustomerRepository();
+            ItemRepository itemRepository = new ItemRepository();
+            PaymentTypeRepository objPaymentTypeRepository = new PaymentTypeRepository();
+
+
+            var objMyltipleModels =  new Tuple<IEnumerable<SelectListItem>, IEnumerable<SelectListItem>, IEnumerable<SelectListItem>>
+                (ObjCustomerRepository.GetAllCustomers() , itemRepository.GetAllItems(), objPaymentTypeRepository.GetAllPaymentType());
+
+
+            return View(objMyltipleModels);
+        }
+
+        [HttpGet]
+        public JsonResult getItemUnitPrice(int? itemId)
+        {
+            decimal Unitprice = objrestaurantDBEntities.Items.SingleOrDefault(model => model.ItemId == itemId).ItemPrice;
+            return Json(Unitprice, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Index(OrderViewModel objOrderViewModel)
+        {
+            OrderRepository objOrderRepository = new OrderRepository();
+            objOrderRepository.AddOrder(objOrderViewModel);
+            return Json(data: " Your Order has been Successfully Placed.", JsonRequestBehavior.AllowGet);
+        }
+    }
+
+
+}
